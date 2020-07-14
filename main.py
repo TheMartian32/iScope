0b4074e48b9541ff353edee0c8bf92ea6eea45c7
Justin Buan <justin_b23@icloud.com>
2020-07-14 13:04:42 -0700

    Made input instructions more clear, and added whitespace to make it more readable.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def ask_for(prompt, error_msg=None, _type=None):
                 continue
         return inp
 
+
 def main():
     """
     Using an angle gauge and a tape measure
@@ -32,14 +33,15 @@ def main():
     print('\n*---------------------------------------*')
     circumference = ask_for(
         'What is the circumference of the base?: ', 'Not an answer', float)
+
     ref_azimuth = ask_for(
-        '\nWhat is the azimuth that you want as a reference point?: ', 'Not an answer', float)
+        '\nWhat is the azimuth that you want as a reference point? (Polaris for example, or M33, in azimuth coordinates.): ', 'Not an answer', float)
 
     marker = ask_for(
-        '\nWhere is the white marker on the tape measure?: ', 'Not an answer', float)
+        '\nWhere is the white marker on the tape measure? (Basically, where is the marker when pointed at the reference point): ', 'Not an answer', float)
 
     tar_azimuth = ask_for(
-        '\nWhat is the target azimuth?: ', 'Not an answer', float)
+        '\nWhat is the target azimuth? (The target you want to see): ', 'Not an answer', float)
     print('*---------------------------------------*')
 
     # * Grabbing degrees, delta actual, and final degrees
@@ -58,17 +60,24 @@ def main():
 
     return print(f'\nPut marker here: {to_rope_point}')
 
+
 if __name__ == "__main__":
     main()
+
+    # * Executes at the end to see if the user wants to repeat the program.
     repeat = ''
     while True:
+
         # * Asks to repeat the script.
         print(
             '\nTyping Y will restart the script, typing N will terminate it.')
+
         repeat = input(
             '\nDo you need any more information? (Y/N): ').lower()
+
         if repeat[0] == 'y':
             main()
             continue
+
         if repeat[0] == 'n':
             break
